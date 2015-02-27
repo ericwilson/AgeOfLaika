807931ac618e0cd442a5e8e373a0278ff0e987aa
Eric Wilson <eric@triadicclosure.com>
2015-02-27 09:54:38 -0800

    adding - if control loop

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -26,11 +26,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /* Each human year equals 10.5 dog years for the first two years, and then 4 dog years for each human year after. 
+    */
+    
     @IBAction func convertToHumanYearsPressed(sender: UIButton) {
         dogYearsLabel.text = "Laika is " + "\(7 * enterHumanYearsTextField.text.toInt()!) in dog years"
         dogYearsLabel.hidden = false
         enterHumanYearsTextField.resignFirstResponder()
     }
+    
+    
+    @IBAction func convertToRealDogYearsPressed(sender: UIButton) {
+        let humanYearsInt = enterHumanYearsTextField.text.toInt()!
+        let firstTwoDogYearsConst = 21
+        
+        if humanYearsInt <= 2 {
+            dogYearsLabel.text = "Laika is " + "\(10.5 * Double(humanYearsInt)) in real dog years"
+        }
+        else {
+            dogYearsLabel.text = "Laika is " + "\(firstTwoDogYearsConst + 4 * (humanYearsInt - 2)) in real dog years"
+        }
+        dogYearsLabel.hidden = false
+    }
+    
 
 }
 
